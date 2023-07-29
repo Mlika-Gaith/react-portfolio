000757f28d10d586064cafb909515e3536f44e93
ghaith_mlika <mlika.gaith@gmail.com>
2023-07-30 00:36:52 +0100

    added banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { CustomNavbar } from "./components/Navbar";
+import { Banner } from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
       <CustomNavbar />
+      <Banner />
     </div>
   );
 }
