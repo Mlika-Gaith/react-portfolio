82c485e11e63ead4da83fdab928eb8a4a3625acc
ghaith_mlika <mlika.gaith@gmail.com>
2023-07-30 20:38:17 +0100

    added skills section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { CustomNavbar } from "./components/Navbar";
 import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
       <CustomNavbar />
       <Banner />
+      <Skills />
     </div>
   );
 }
